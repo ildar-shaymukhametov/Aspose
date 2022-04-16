e90afe6181323db84869b86b37bf9579ea86b40d
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-04-16 20:20:48 +0500

    add translation api

---
+++ Translation.Api/src/Controllers/TranslationController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TranslationController : ControllerBase
+{
+    private readonly ILogger<TranslationController> _logger;
+
+    public TranslationController(ILogger<TranslationController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("translate")]
+    public IActionResult Translate(string text)
+    {
+        return Ok();
+    }
+}
