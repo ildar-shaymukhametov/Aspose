272610cf0fcfbe8abacabfbc17e22620afad7c12
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-04-16 20:47:22 +0500

    remove redundant

--- Translation.Api/Api/Controllers/TranslationController.cs
+++ Translation.Api/Api/Controllers/TranslationController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace src.Controllers;
 
 [ApiController]
-[Route("[controller]")]
 public class TranslationController : ControllerBase
 {
     private readonly ILogger<TranslationController> _logger;
