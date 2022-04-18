ae332ac2f42749bb386749b560d85e6242618d48
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-04-18 08:38:35 +0500

    update console

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    if (args.Length < 3)
+    {
+        Console.WriteLine("Required arguments not provided");
+        return;
+    }
+
+    var filename = args[0];
+
+    using var httpClient = new HttpClient
+    {
+        BaseAddress = new Uri("https://localhost:5001/translate")
+    };
+    httpClient.DefaultRequestHeaders.Add("X-Translation-Api-Key", "***");
+
+    var translationClient = new TranslationApiClient(httpClient);
+    var texts = new [] { "house" };
+    var sourceLanguage = args[1];
+    var targetLanguage = args[2];
+
+    var translations = await translationClient.TranslateAsync(texts, sourceLanguage, targetLanguage);
+    if (translations == null)
+    {
+        Console.WriteLine($"Unable to translate text: {string.Join(", ", texts)}");
+        return;
+    }
+
+    foreach (var item in translations)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.GetBaseException()?.Message}");
+}
